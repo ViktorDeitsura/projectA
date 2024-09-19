09acffc254bacde8c06077ee180486b901a5392b
Viktor Deitsura <ViktorDeitsura@gmail.com>
2024-09-19 22:30:28 +0700

    js logic

--- admin.php
+++ admin.php
@@ -55,7 +55,6 @@ if (!$user->isAdmin) die('Доступ закрыт');
         </div>
     </div>
 
-
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script type="text/javascript" src="assets/js/admin.js"></script>
 </body>
